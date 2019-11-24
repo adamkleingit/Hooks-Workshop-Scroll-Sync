5b5a1874beb7a35589d5b403d2925b90ddbf485c
Adam Klein <adam@500tech.com>
2019-11-24 18:48:32 +0100

    solution

--- src/index.js
+++ src/index.js
@@ -1,14 +1,45 @@
-import React from "react";
+import React, { useState, useRef, useLayoutEffect } from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
+function ScrollSync({ scrollTop, setScrollTop, children }) {
+  const ref = useRef();
+
+  useLayoutEffect(function syncScroll() {
+    ref.current.scrollTop = scrollTop;
+  }, [scrollTop]);
+
+  const onScroll = (e) => {
+    setScrollTop(e.target.scrollTop);
+  };
+  return (
+    <div
+      className="App"
+      ref={ref}
+      onScroll={onScroll}>
+      {children}
+    </div>
+  );
+}
+
+function useScrollSync(initialValue) {
+  const [scrollTop, setScrollTop] = useState(initialValue);
+
+  return {
+    scrollTop,
+    setScrollTop
+  }
+}
+
+
 function App() {
+  const scrollSyncState = useScrollSync(0);
   // Create a component that allows to read and set the scroll position of the element
   // And sync the state between two containers
   return (
     <div>
-      Scroll Top: {null} <button onClick={null}>Top</button>
-      <div className="App">
+      Scroll Top: {scrollSyncState.scrollTop} <button onClick={() => scrollSyncState.setScrollTop(0)}>Top</button>
+      <ScrollSync {...scrollSyncState}>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
@@ -36,7 +67,38 @@ function App() {
           pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
           culpa qui officia deserunt mollit anim id est laborum
         </p>
-      </div>
+      </ScrollSync>
+
+      <ScrollSync {...scrollSyncState}>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum
+        </p>
+      </ScrollSync>
+
     </div>
   );
 }
